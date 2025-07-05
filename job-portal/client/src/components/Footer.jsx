1ab8a93ba24a3746642d625ccb963666af7ad327
sharma2612 <144610168+sharma2612@users.noreply.github.com>
2025-07-05 18:05:55 +0530

    Added icons in Footer.jsx

--- job-portal/client/src/components/Footer.jsx
+++ job-portal/client/src/components/Footer.jsx
@@ -6,9 +6,11 @@ const Footer = () => {
       <img width={160} src={assets.logo} alt="" />
       <p className='flex-1 border-l border-gray-400 pl-4 text-sm text-gray-500 max-sm:hidden'>Copyright @GreatStack.dev | All right reserved.</p>
       <div className='flex gap-2.5'>
+       
         <img width={38} src={assets.facebook_icon} alt="" />
         <img width={38} src={assets.twitter_icon} alt="" />
         <img width={38} src={assets.instagram_icon} alt="" />
+        
       </div>
     </div>
   )
